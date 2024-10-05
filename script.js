280ac706da076a40146c7f0d5dcbf02072041e3c
Alex Fernandes <alexfernands@outlook.com>
2024-10-05 18:51:00 -0400

    functions delete

--- script.js
+++ script.js
@@ -3,5 +3,14 @@ function firstClick(message) {
 
 var inputText = document.getElementById("userInput").value
 
-    alert(inputText);
+var p = document.createElement("p")
+
+p.textContent = inputText
+
+document.getElementById("resultContainer").appendChild(p)
+
+}
+
+function deleteInput() {
+    document.getElementById("resultContainer").innerHTML = ""
 }
